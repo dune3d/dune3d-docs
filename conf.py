25f15fa763433ec1e8d94ebb33da573259de0a1a
magdesign <magdesign@disroot.org>
2025-12-25 00:14:01 +0200

    Revert "add logo"
    This reverts commit 078536a1dd8008167e2a48d9f1514f7c7e8aa98d.

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'Lukas K.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.3'
+release = '1.2'
 
 
 # -- General configuration ---------------------------------------------------
@@ -76,8 +76,6 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {'logo_only': True,}
-html_logo = "images/logo.svg"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
