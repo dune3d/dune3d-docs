4f48d5e627d6a6ee075cb72f8f7ba05fc4727d01
magdesign <magdesign@disroot.org>
2026-01-13 21:52:49 +0200

    add logo to sidebar
    created a logo version with gray text

--- conf.py
+++ conf.py
@@ -76,6 +76,8 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {'logo_only': True,}
+html_logo = "images/logo.svg"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
